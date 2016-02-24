1563401cee7220ad95b0c6c7862d479d339ff792
Ben Jacobs <ben.jacobs@vanti.co.uk>
2016-02-24 01:12:07 +0000

    add some helper functions for setting channels and volume.

--- test/test.js
+++ test/test.js
@@ -28,3 +28,12 @@ var remote = new Test({commandInterval: 100});
 for(let i = 0; i < 10; i++) {
   remote.send(i);
 }
+
+//test set channel
+remote.setChannel(104);
+
+//test volume and channel
+remote.volume(true);
+remote.volume(false);
+remote.channel(true);
+remote.channel(false);

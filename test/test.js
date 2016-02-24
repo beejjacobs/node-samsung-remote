ed806f911503c212add7099e25ce435c3035ab62
Ben Jacobs <ben.jacobs@vanti.co.uk>
2016-02-24 00:40:25 +0000

    add test and script

---
+++ test/test.js
@@ -0,0 +1,30 @@
+'use strict';
+var Samsung = require('./../lib/node-samsung-remote');
+
+class Test extends Samsung {
+  constructor(config) {
+    super(config);
+  }
+
+  //override the send to device to just log commands
+  sendToDevice(command) {
+    this.processing = true;
+
+    console.log(command);
+
+    let self = this;
+    //Success, process next
+    self.timer = setTimeout(function() {
+      self.timer = null;
+      self.processing = false;
+      self.processBuffer();
+    }, self.commandInterval);
+  }
+}
+
+//test the command buffer and commandInterval
+var remote = new Test({commandInterval: 100});
+
+for(let i = 0; i < 10; i++) {
+  remote.send(i);
+}
